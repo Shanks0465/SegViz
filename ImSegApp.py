634cb01a81e97595e25e261059dd7df1214ec643
Shanks0465 <umashanks99@gmail.com>
2020-10-03 17:00:32 +0530

    Added Python Script

---
+++ ImSegApp.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import colorchooser
+from tkinter import messagebox
+import cv2
+import numpy as np
+from tkinter import filedialog
+import os
+
+App = Tk()
+App.iconbitmap(default='transparent.ico')
+mainloop()
