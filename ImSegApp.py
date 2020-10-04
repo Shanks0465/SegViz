8189c438855471d3dfd80f03f10db3b92e364d58
Shanks0465 <umashanks99@gmail.com>
2020-10-05 02:51:40 +0530

    Code Updated with Canvas Draw Functions

--- ImSegApp.py
+++ ImSegApp.py
@@ -9,6 +9,15 @@ import cv2
 from PIL import Image, ImageTk
 import numpy as np
 import os
+#--------------------------------------------------------------------------------------------------
+
+
+
+global array
+global array2
+array=[]
+array2=[]
+
 
 #--------------------------------------------------------------------------------------------------
 
@@ -35,6 +44,7 @@ def selectfolder():
 
 def showimg(event):
     n = directoryview.curselection()
+    global fname,img
     fname = directoryview.get(n)
     imsegcanvas.delete("all")
     imgpath=filename+"/"+fname
@@ -44,10 +54,61 @@ def showimg(event):
     img = ImageTk.PhotoImage(img)
     imsegcanvas.config(width=imgwidth,height=imgheight,scrollregion=(0,0,imgwidth,imgheight))
     imsegcanvas.create_image(0,0, anchor=NW, image=img)
-    imsegcanvas.image=img
 
 
-#--------------------------------------------------------------------------------------------------
+
+def choose_color():
+    global color_code
+    global clf
+    clf = colorchooser.askcolor(title="Choose color")
+    color_code=clf[1]
+    print(color_code)
+
+
+
+
+def point(event):
+    try:
+        x1, y1 = (imsegcanvas.canvasx(event.x) - 1), (imsegcanvas.canvasy(event.y) - 1)
+        imsegcanvas.create_oval(x1 - 2, y1 - 2, x1 + 2, y1 + 2, fill="#ff0000")
+        array.append(x1)
+        array.append(y1)
+        array2.append([x1, y1])
+    except:
+        messagebox.showerror("Error", "Error Occured")
+
+def clearcanvas(event):
+    imsegcanvas.delete("all")
+    imsegcanvas.create_image(0, 0, anchor=NW, image=img)
+    imsegcanvas.image = img
+    messagebox.showinfo("Message", "Segmap Cleared")
+
+def genpolygon(event):
+    try:
+        imsegcanvas.create_polygon(array, outline=color_code, fill=color_code, width=3, stipple="gray50")
+        # pts = np.array(array2, np.int32)
+        # pts = pts.reshape((-1, 1, 2))
+        # cv2.fillPoly(segmap, [pts], [clf[0][2],clf[0][1],clf[0][0]], 1)
+        array2.clear()
+        array.clear()
+    except:
+        messagebox.showerror("Error", "Choose Color")
+
+
+
+def outline(event):
+    try:
+        x1, y1 = (imsegcanvas.canvasx(event.x) - 1), (imsegcanvas.canvasy(event.y) - 1)
+        imsegcanvas.create_oval(x1 - 1, y1 - 1, x1 + 1, y1 + 1, fill="#ff0000")
+        array.append(x1)
+        array.append(y1)
+        array2.append([x1, y1])
+    except:
+        messagebox.showerror("Error", "Error Occured")
+
+
+
+    #--------------------------------------------------------------------------------------------------
 
 # Landing Page
 global landingPage
@@ -55,11 +116,11 @@ landingPage = Frame(App)
 landingText = Label(landingPage,text="An Image Segmentation Tool using Tkinter and OpenCV")
 selectFolder = Button(landingPage,text="Select Image Folder",command=selectfolder)
 canvas = Canvas(landingPage, width = 300, height = 300)
-img = Image.open("Segvizlogo.png")
-img = img.resize((300, 300), Image.ANTIALIAS)
-img=ImageTk.PhotoImage(img)
+imgland = Image.open("Segvizlogo.png")
+imgland = imgland.resize((300, 300), Image.ANTIALIAS)
+imgland=ImageTk.PhotoImage(imgland)
 
-canvas.create_image(20,20, anchor=NW, image=img)
+canvas.create_image(20,20, anchor=NW, image=imgland)
 canvas.pack()
 selectFolder.pack(side=BOTTOM,fill=BOTH)
 landingText.pack(fill=BOTH,side=BOTTOM)
@@ -88,7 +149,7 @@ imsegcanvas.create_image(0,0, anchor=NW, image=canvasimage)
 global directoryview
 directoryview=Listbox(imsegpage)
 directoryview.bind("<<ListboxSelect>>", showimg)
-directoryview.pack(side="left", fill=Y)
+directoryview.pack(side="left", fill=Y,expand=False)
 
 # Scrollbars for Image
 scroll_x = Scrollbar(imsegpage, orient="horizontal", command=imsegcanvas.xview)
@@ -100,14 +161,21 @@ scroll_y.pack(side=RIGHT,fill=Y)
 # Tab Control
 tabControl = ttk.Notebook(imsegpage)
 tab1 = ttk.Frame(tabControl)
-selectcolor = Button(tab1,text="Select Color")
+selectcolor = Button(tab1,text="Select Color",command=choose_color)
 save = Button(tab1,text="Save Segmentation")
 tabControl.add(tab1, text='Tools')
 
+# Pack the widgets
 selectcolor.pack(fill=BOTH)
 save.pack(fill=BOTH)
 tabControl.pack(side=TOP,fill=X)
 
+# Bind the canvas actions
+
+imsegcanvas.bind("<Button-1>",point)
+imsegcanvas.bind("<Button-3>",genpolygon)
+imsegcanvas.bind("<B1-Motion>",outline)
+imsegcanvas.bind("<Button-2>",clearcanvas)
 #--------------------------------------------------------------------------------------------------
 
 imsegcanvas.pack()
