02c15f11cc4910fa85decacf21fc065217d94812
Shanks0465 <umashanks99@gmail.com>
2020-10-04 15:37:51 +0530

    Code Updated

--- ImSegApp.py
+++ ImSegApp.py
@@ -20,14 +20,17 @@ App.geometry("400x400")
 
 # Functions and Actions
 
-
+def selectfolder():
+    landingPage.destroy()
+    imsegpage.pack(fill=BOTH)
 
 #--------------------------------------------------------------------------------------------------
 
 # Landing Page
+global landingPage
 landingPage = Frame(App)
 landingText = Label(landingPage,text="An Image Segmentation Tool using Tkinter and OpenCV")
-selectFolder = Button(landingPage,text="Select Image Folder")
+selectFolder = Button(landingPage,text="Select Image Folder",command=selectfolder)
 canvas = Canvas(landingPage, width = 300, height = 300)
 img = Image.open("Segvizlogo.png")
 img = img.resize((300, 300), Image.ANTIALIAS)
@@ -37,24 +40,25 @@ canvas.create_image(20,20, anchor=NW, image=img)
 canvas.pack()
 selectFolder.pack(side=BOTTOM,fill=BOTH)
 landingText.pack(fill=BOTH,side=BOTTOM)
-# landingPage.pack()
+landingPage.pack()
 
 #--------------------------------------------------------------------------------------------------
 
 
 # Image Segmentation Tool
+global imsegpage
 imsegpage = Frame(App)
 imsegcanvas = Canvas(imsegpage,width=1000,height=1000,scrollregion=(0,0,1000,1000))
 
 # Scrollbars for Image
-scroll_x = Scrollbar(App, orient="horizontal", command=canvas.xview)
-scroll_y = Scrollbar(App, orient="vertical", command=canvas.yview)
+scroll_x = Scrollbar(imsegpage, orient="horizontal", command=canvas.xview)
+scroll_y = Scrollbar(imsegpage, orient="vertical", command=canvas.yview)
 imsegcanvas.configure(yscrollcommand=scroll_y.set, xscrollcommand=scroll_x.set)
 scroll_x.pack(side=BOTTOM,fill=X)
 scroll_y.pack(side=RIGHT,fill=Y)
 
 # Tab Control
-tabControl = ttk.Notebook(App)
+tabControl = ttk.Notebook(imsegpage)
 tab1 = ttk.Frame(tabControl)
 selectcolor = Button(tab1,text="Select Color")
 save = Button(tab1,text="Save Segmentation")
@@ -62,12 +66,11 @@ tabControl.add(tab1, text='Tools')
 
 selectcolor.pack(fill=BOTH)
 save.pack(fill=BOTH)
-tabControl.pack(side=LEFT,fill=Y)
+tabControl.pack(side=TOP,fill=X)
 
 #--------------------------------------------------------------------------------------------------
 
 imsegcanvas.pack()
-imsegpage.pack(fill=BOTH)
 
 
 mainloop()
